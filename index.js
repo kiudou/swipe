e983f93e6c97f914700b723dd8e0ec7c188366c7
qidong <qidong@xiaomi.com>
2020-01-20 10:22:51 +0800

    feat: 加入过渡动画，无缝轮播，自动播放的效果

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
 let swipePic = document.querySelector('.swipe__pic');
+let swipePicChildNode = swipePic.children;
 let arrowLeft = document.querySelector('.swipe__arrow-left');
 let arrowRight = document.querySelector('.swipe__arrow-right');
 
 arrowLeft.onclick = () => {
-    let styleLeft = parseInt(swipePic.style.left) - 200;
-    swipePic.style.left = (styleLeft <= -800 ? styleLeft + 800 : styleLeft) + 'px';
+    setTransitionDuration(durationTime + 'ms');
+
+    let styleLeft = parseInt(swipePic.style.left) - singlePicWidth;
+
+    setLeftWidth(styleLeft);
+
+    // 在轮播完之后进行位置的切换，不然会影响时候轮播时候过渡动画时间
+    setTimeout(() => setPosition(styleLeft), durationTime);
+
 }
 
 arrowRight.onclick = () => {
-    let styleLeft = parseInt(swipePic.style.left) + 200;
-    swipePic.style.left = (styleLeft > 0 ? styleLeft - 800 : styleLeft) + 'px';
+    setTransitionDuration(durationTime + 'ms');
+
+    let styleLeft = parseInt(swipePic.style.left) + singlePicWidth;
+
+    setLeftWidth(styleLeft);
+
+    setTimeout(() => setPosition(styleLeft), durationTime);
 }
+
+// 图片数量
+const number = 4
+
+// 单个图片的宽度
+const singlePicWidth = 200;
+
+// 总的宽度
+const swipeWidth = (number + 2) * singlePicWidth;
+
+//动画过渡时间
+const durationTime = 1000;
+
+// 设置过渡动画时间
+const setTransitionDuration = (duration) => {
+    swipePic.style.webkitTransitionDuration = duration;
+}
+
+const cloneNode = (elem, index) => {
+
+    //复制节点，放到最后
+    nodeTmp = swipePicChildNode[index].cloneNode(true);
+    elem.appendChild(nodeTmp);
+}
+
+const setLeftWidth = (value) => {
+    swipePic.style.left = value + 'px';
+}
+
+// 方便无缝轮播
+const setPosition = (currrentPosition) => {
+    if (currrentPosition == (singlePicWidth - swipeWidth)) {
+        setTransitionDuration('0s');
+        setLeftWidth(-singlePicWidth);
+    }
+
+    if(currrentPosition == 0) {
+        setTransitionDuration('0s');
+        setLeftWidth(2*singlePicWidth - swipeWidth);
+    }
+    return;
+}
+
+const init = () => {
+    swipePic.style.left = '0px';
+
+    // 原来的节点上，最后增加两个头节点，方便进行轮播
+    cloneNode(swipePic, 0);
+    cloneNode(swipePic, 1);
+
+    // 自动轮播
+    setInterval(() => arrowLeft.onclick(), durationTime + 1000);
+}
+
+init();
